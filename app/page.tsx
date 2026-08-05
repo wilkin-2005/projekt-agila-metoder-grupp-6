fa11b3c6aa9b076293db61a119f834b6e87afe11
Georgij Li <ligeorgij7@hotmail.com>
2026-08-05 14:54:44 +0200

    Create ListTable for products

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ListTable from "./components/ListTable";
 import type { ProductsResponse } from "./types";
 
 const API_URL = "http://localhost:4000";
@@ -18,6 +19,7 @@ console.log(products);
   return (
     <main>
       <h1>Products</h1>
+      <ListTable data={products} columns={["title","brand","category","stock", "price"]}/>
       <div>{products.map((product) => <h2 key={product.id}>{product.title} - {product.category?.name}</h2>)}</div>
     </main>
   );
