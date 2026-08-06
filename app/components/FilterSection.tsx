a5c836f0d2f9ea4693e1a22511add0952c79087c
leo98lxl <279720782+leo98lxl@users.noreply.github.com>
2026-08-06 09:50:34 +0200

    added categories dropdown and filter button

--- app/components/FilterSection.tsx
+++ app/components/FilterSection.tsx
@@ -4,12 +4,19 @@ export default function FilterSection() {
         <form action="" method="get">
             <input type="text" id="search" name="search" placeholder="Search products">
             </input>
+            <select id="categories" name="categories">
+                <option value="categories-all">All Categories</option>
+                <option value="shoes-women">Women's Shoes</option>
+                <option value="watches-women">Women's Watches</option>
+            </select>
             
             <select id="stock" name="stock">
                 <option value="stock-all" selected>All Stock</option>
                 <option value="in-stock">In stock</option>
                 <option value="out-stock">Out of stock</option>
             </select>
+
+            <button className="filter-button">Filter</button>
         </form>
     </section>
     )
