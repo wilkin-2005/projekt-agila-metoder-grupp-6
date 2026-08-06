616c95890a2ed4719549a314cf4de5f99af57f14
Georgij Li <ligeorgij7@hotmail.com>
2026-08-06 21:57:48 +0200

    make sure same nr of pages is shown every time

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -13,21 +13,24 @@ export default function Pagination({page, pages}: {page:number,pages:number}){
     arr.length = pages;
     arr.fill(0);
 
-    const visiblePagesOffsetFromMiddle = 2; // If you change this value you will need to change values in other places below to make it work
+    const visiblePagesOffsetFromMiddle = 1;
 
     const reduceOffsetStart = currentPage-visiblePagesOffsetFromMiddle === 2 ? 1: 0;
     const reduceOffsetEnd = currentPage+visiblePagesOffsetFromMiddle === pages-1 ? 1 : 0;
     const offsetStart = currentPage <= visiblePagesOffsetFromMiddle ? (visiblePagesOffsetFromMiddle+1)-currentPage : 0;
     const offsetEnd = currentPage > pages-(visiblePagesOffsetFromMiddle+1) ? visiblePagesOffsetFromMiddle-(pages-currentPage) : 0;
+    const extraStart =  currentPage-visiblePagesOffsetFromMiddle <= 2;
+    const extraEnd = currentPage+visiblePagesOffsetFromMiddle >= pages-1;
 
     return <ul className='Pagination'>
         {arr.map(((v,i) =>{
-            const pageVisible = (currentPage-(i+1) <= visiblePagesOffsetFromMiddle+offsetEnd-reduceOffsetEnd && currentPage-(i+1) >= 0) || (currentPage-(i+1) >= -visiblePagesOffsetFromMiddle-offsetStart+reduceOffsetStart && currentPage-(i+1) <= 0)
+            const pageVisible = (currentPage-(i+1) <= visiblePagesOffsetFromMiddle+offsetEnd-reduceOffsetEnd+Number(extraEnd) && currentPage-(i+1) >= 0) ||
+             (currentPage-(i+1) >= -visiblePagesOffsetFromMiddle-offsetStart+reduceOffsetStart-Number(extraStart) && currentPage-(i+1) <= 0)
             
             const firstPageExtraShouldBeVisible = !pageVisible && i+1 === 1;
             const lastPageExtraShouldBeVisible = !pageVisible && i+1 === pages;
             return pageVisible || firstPageExtraShouldBeVisible || lastPageExtraShouldBeVisible?<Fragment key={i+1}>
-                {lastPageExtraShouldBeVisible && currentPage+visiblePagesOffsetFromMiddle !== pages-1 ? <li className='Pagination__ExtraPage'><p>...</p></li>:null}
+                {lastPageExtraShouldBeVisible && currentPage+visiblePagesOffsetFromMiddle !== pages-1 && pages !== visiblePagesOffsetFromMiddle*2+1+2 ? <li className='Pagination__ExtraPage'><p>...</p></li>:null}
          <li key={i+1} onClick={() =>{if(currentPage === i+1) return;
                     const params = new URLSearchParams();
                     params.set("page", (i+1).toString());
@@ -37,7 +40,7 @@ export default function Pagination({page, pages}: {page:number,pages:number}){
             
             <p>{i+1}</p>
         </li>
-        {firstPageExtraShouldBeVisible && currentPage-visiblePagesOffsetFromMiddle !== 2 ? <li className='Pagination__ExtraPage'><p>...</p></li>:null}
+        {firstPageExtraShouldBeVisible && currentPage-visiblePagesOffsetFromMiddle !== 2 && pages !== visiblePagesOffsetFromMiddle*2+1+2 ? <li className='Pagination__ExtraPage'><p>...</p></li>:null}
         </Fragment>: null}))}
     </ul>
 }
