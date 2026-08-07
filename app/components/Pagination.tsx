52b90cefa0b0e5b903ccbb54781f03474f14fd78
Georgij Li <ligeorgij7@hotmail.com>
2026-08-07 06:54:33 +0200

    use variables

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -17,30 +17,37 @@ export default function Pagination({page, pages}: {page:number,pages:number}){
 
     const reduceOffsetStart = currentPage-visiblePagesOffsetFromMiddle === 2 ? 1: 0;
     const reduceOffsetEnd = currentPage+visiblePagesOffsetFromMiddle === pages-1 ? 1 : 0;
-    const offsetStart = currentPage <= visiblePagesOffsetFromMiddle ? (visiblePagesOffsetFromMiddle+1)-currentPage : 0;
-    const offsetEnd = currentPage > pages-(visiblePagesOffsetFromMiddle+1) ? visiblePagesOffsetFromMiddle-(pages-currentPage) : 0;
-    const extraStart =  currentPage-visiblePagesOffsetFromMiddle <= 2;
-    const extraEnd = currentPage+visiblePagesOffsetFromMiddle >= pages-1;
+    const extraOffsetStart = currentPage <= visiblePagesOffsetFromMiddle ? (visiblePagesOffsetFromMiddle+1)-currentPage : 0;
+    const extraOffsetEnd = currentPage > pages-(visiblePagesOffsetFromMiddle+1) ? visiblePagesOffsetFromMiddle-(pages-currentPage) : 0;
+    const extraStart =  currentPage-visiblePagesOffsetFromMiddle <= 2; // when first page is included to keep same number of pages all the time
+    const extraEnd = currentPage+visiblePagesOffsetFromMiddle >= pages-1; // when last page is included to keep same number of pages all the time
+
+    const offsetLeft = visiblePagesOffsetFromMiddle+extraOffsetEnd-reduceOffsetEnd+Number(extraEnd);
+    const offsetRight = -(visiblePagesOffsetFromMiddle+extraOffsetStart-reduceOffsetStart+Number(extraStart));
+
+    const pageLimitStart = currentPage-offsetLeft;
+    const pageLimitEnd = currentPage-offsetRight;
 
     return <ul className='Pagination'>
-        {arr.map(((v,i) =>{
-            const pageVisible = (currentPage-(i+1) <= visiblePagesOffsetFromMiddle+offsetEnd-reduceOffsetEnd+Number(extraEnd) && currentPage-(i+1) >= 0) ||
-             (currentPage-(i+1) >= -visiblePagesOffsetFromMiddle-offsetStart+reduceOffsetStart-Number(extraStart) && currentPage-(i+1) <= 0)
+        {arr.map(((v,i) =>{ 
+            const pageVisible = (((i+1) >= pageLimitStart) && ((i+1) <= pageLimitEnd))
             
             const firstPageExtraShouldBeVisible = !pageVisible && i+1 === 1;
             const lastPageExtraShouldBeVisible = !pageVisible && i+1 === pages;
-            return pageVisible || firstPageExtraShouldBeVisible || lastPageExtraShouldBeVisible?<Fragment key={i+1}>
-                {lastPageExtraShouldBeVisible && currentPage+visiblePagesOffsetFromMiddle !== pages-1 && pages !== visiblePagesOffsetFromMiddle*2+1+2 ? <li className='Pagination__ExtraPage'><p>...</p></li>:null}
-         <li key={i+1} onClick={() =>{if(currentPage === i+1) return;
-                    const params = new URLSearchParams();
-                    params.set("page", (i+1).toString());
-                    router.push(pathname + '?' +params.toString())
-         }} 
-         className={`Pagination__Item ${currentPage === i+1 ? 'Pagination__Item--current': ''}`}>
             
-            <p>{i+1}</p>
-        </li>
-        {firstPageExtraShouldBeVisible && currentPage-visiblePagesOffsetFromMiddle !== 2 && pages !== visiblePagesOffsetFromMiddle*2+1+2 ? <li className='Pagination__ExtraPage'><p>...</p></li>:null}
-        </Fragment>: null}))}
+            return pageVisible || firstPageExtraShouldBeVisible || lastPageExtraShouldBeVisible?
+            <Fragment key={i+1}>
+                {lastPageExtraShouldBeVisible && pageLimitEnd < pages-1  ? <li className='Pagination__ExtraPage'><p>...</p></li>:null}
+                <li onClick={() =>{if(currentPage === i+1) return;
+                            const params = new URLSearchParams();
+                            params.set("page", (i+1).toString());
+                            router.push(pathname + '?' +params.toString())
+                }} 
+                className={`Pagination__Item ${currentPage === i+1 ? 'Pagination__Item--current': ''}`}>
+                    
+                    <p>{i+1}</p>
+                </li>
+                {firstPageExtraShouldBeVisible && pageLimitStart > 2 ? <li className='Pagination__ExtraPage'><p>...</p></li>:null}
+            </Fragment>: null}))}
     </ul>
 }
