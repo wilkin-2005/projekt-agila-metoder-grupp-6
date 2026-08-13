fde505ebc6468663cc4b4a8ea91ec4a2357d892d
Georgij Li <ligeorgij7@hotmail.com>
2026-08-13 11:32:30 +0200

    add "add product" page

---
+++ app/product/add/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import './page.css'
+
+export default function Page() {
+  return (
+    <main className="AddProduct">
+        <section className="centered-section">
+            <div className='AddProduct__Header'>
+        <h1 className="AddProduct__Title">
+            Add product
+        </h1>
+        <a href='/'>Go to product page</a>
+        </div>
+        <section>
+            {
+                /* add product form here */
+            }
+        </section>
+        </section>
+    </main>
+  );
+}
