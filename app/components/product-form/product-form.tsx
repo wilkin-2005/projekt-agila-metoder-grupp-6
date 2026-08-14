2efe1337beefcd46caa938d82d0e7c0d24d4de71
wilkin2005 <279715150+wilkin-2005@users.noreply.github.com>
2026-08-14 16:13:20 +0200

    • Added many more input fields to the add/edit product-form. • Added some more styling for the SKU input fields. • Had help from Leo with the work on this commit.
    Co-authored-by: Leo Leksell <leo98lxl@users.noreply.github.com>

--- app/components/product-form/product-form.tsx
+++ app/components/product-form/product-form.tsx
@@ -2,14 +2,7 @@
 import "./product-form.css";
 import { Product } from "@/app/types";
 
-/* Product information atributes:
-    price: number;
-    discountPercentage?: number;
-    rating?: number;
-    stock?: number;
-    tags?: string[];
-    brand?: string;
-    sku?: string;
+/* Product information attributes:
     weight?: number;
     dimensions?: {
         width: number;
@@ -18,7 +11,8 @@ import { Product } from "@/app/types";
     };
     warrantyInformation?: string;
     shippingInformation?: string;
-    availabilityStatus?: string;
+    
+    
     reviews?: {
         rating: number;
         comment: string;
@@ -52,27 +46,26 @@ export default function ProductForm()
 
                 {/* id: number; */}
 
-                {/* TITLE */}
+                {/* title: string; */}
                 <div className="form-group">
                     <label htmlFor="title">Title</label>
-                    <input type="text" id="title" name="title" placeholder="Product title..." maxLength={50} required autoFocus />
+                    <input type="text" id="title" name="title" placeholder="Product title (max 50 characters)" maxLength={50} required autoFocus />
                 </div>
 
-                {/* DESCRIPTION */}
+                {/* description: string; */}
                 <div className="form-group">
                     <label htmlFor="description">Description</label>
-                    <input type="text" id="description" name="description" placeholder="Product description..." maxLength={250} />
+                    <input type="text" id="description" name="description" placeholder="Product description (max 250 characters)" maxLength={250} />
                 </div>
 
                 {/* categoryId: number; */}
 
                 {/* category?: Category; */}
                 <div className="form-group">
-                    {/* <input type="text" id="category" name="category" placeholder="Product category..." /> */}
-                    
                     <label htmlFor="category">Category</label>
+
                     <select id="category" name="category" defaultValue="default" >
-                        <option value="default" disabled > Chose category </option>
+                        <option value="default" disabled > Choose category </option>
                         <option value="1"> Beauty </option>
                         <option value="2"> Fragrance </option>
                         <option value="3"> Furniture </option>
@@ -98,21 +91,132 @@ export default function ProductForm()
                         <option value="23"> Woman's Shoes </option>
                         <option value="24"> Woman's Watches </option>
                     </select>
-
                 </div>
 
+                {/* price: number; */}
+                <div className="form-group">
+                    <label htmlFor="price">Price in euros</label>
+                    <input type="number" id="price" name="price" placeholder="Price in €" />
+                </div>
 
-                <button type="submit" className="btn-submit" >Submit</button>
+                {/* discountPercentage?: number; */}
+                <div className="form-group">
+                    <label htmlFor="discountPercentage" > Discount in percent </label>
+                    <input type="number" id="discountPercentage" name="discountPercentage" placeholder="Discount in %" />
+                </div>
+
+                {/* rating?: number; */}
+                {/* <div className="form-group" >
+                    <label htmlFor="rating" > Rating from 1-5 </label>
+                    <input type="range" id="rating" name="rating" placeholder="Rating (1-5)" min={1} max={5} />
+                </div> */}
+                
+                {/* stock?: number; */}
+                <div className="form-group" >
+                    <label htmlFor="stock" > Products in stock </label>
+                    <input type="number" id="stock" name="stock" placeholder="Number of products in stock..." />
+                </div>
+                
+
+                {/* tags?: string[]; */}
+                <div className="form-group" >
+                    <label htmlFor="tags1" > Add 1-3 tags </label>
+                    <input type="text" id="tags1" name="tag1" placeholder="Tag 1 (required)" required />
+
+
+                    <input type="text" name="tag2" placeholder="Tag 2 (optional)" />
+                    <input type="text" name="tag3" placeholder="Tag 3 (optional)" />
+                </div>
+
+                {/* brand?: string; */}
+                <div className="form-group" >
+                    <label htmlFor="brand" > The products brand </label>
+                    <input type="text" id="brand" name="brand" placeholder="Product brand..." />
+                </div>
+
+                {/* sku?: string; */}
+                <div className="form-group" >
+                    <label htmlFor="sku" > Stock-keeping unit (SKU) </label>
+                    
+                    <div className="sku-input" >
+                        <input type="text" name="sku1" id="sku" placeholder="ABC"
+                            pattern="[A-Z]{3}" title="Three uppercase letters, A-Z" />
+                        <span> - </span>
+
+                        <input type="text" name="sku2" placeholder="DEF"
+                            pattern="[A-Z]{3}" title="Three uppercase letters, A-Z" />
+                        <span> - </span>
+
+                        <input type="text" name="sku3" placeholder="GHI"
+                            pattern="[A-Z]{3}" title="Three uppercase letters, A-Z" />
+                        <span> - </span>
+
+                        <input type="text" name="sku4" placeholder="123"
+                            pattern="[0-9]{3}" title="Three numbers, 0-9" />
+                    </div>
+                </div>
+                
+
+
+                
+
+                {/* availabilityStatus: string */}
+
+                <div className="form-group" >
+                    <label htmlFor="availability" > Availability </label>
+                    <select id="availability" name="availability" defaultValue="default">
+                        <option value="default" disabled>Set availability</option>
+                        <option value="in-stock">In Stock</option>
+                        <option value="low-stock">Low Stock</option>
+                        <option value="out-of-stock">Out of Stock</option>
+                    </select>
+                </div>
+
+                {/* reviews?: {
+        rating: number;
+        comment: string;
+        date: string;
+        reviewerName: string;
+        reviewerEmail: string;
+                }[]; */}
+
+                <div className="form-group" >
+                    <h2>Product Review</h2>
+
+                    <label htmlFor="review-rating">Rating</label>
+                    <input type="number" id="review-rating" name="review-rating" min="1" max="5" placeholder="Rating (1-5)" />
+
+                    <label htmlFor="review-comment">Comment</label>
+                    <textarea id="review-comment" name="review-comment" placeholder="Leave a comment..."></textarea>
+
+                    <label htmlFor="review-date">Date</label>
+                    <input type="date" id="review-date" name="review-date" defaultValue="2026-08-14" />
+
+                    <label htmlFor="reviewer-name">Your name</label>
+                    <input type="text" id="reviewer-name" name="reviewer-name" placeholder="Name of reviewer" />
+
+                    <label htmlFor="reviewer-email">Your email</label>
+                    <input type="email" id="reviewer-email" name="reviewer-email" placeholder="Email of reviewer" />
+                </div>
+
+                {/* returnPolicy?: string; */}
+
+                <div className="form-group" >
+                    <label htmlFor="return-policy">Return policy</label>
+                </div>
+
+                {/* SUBMIT BUTTON */}
+
+                <button type="submit" className="btn-submit" > Submit </button>
                     
             </form>
 
         </section>
     );
 }
-
 /* 
-                <div className="form-group">
-                    <label htmlFor="generic">generic form group</label>
+                <div className="form-group" >
+                    <label htmlFor="generic" > generic form group </label>
                     <input type="text" id="generic" name="generic" placeholder="..." />
                 </div>
  */
