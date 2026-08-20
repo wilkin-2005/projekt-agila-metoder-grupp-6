8bd8e5223ac45919a1cf5be9f0cf27d5688d0eda
wilkin2005 <279715150+wilkin-2005@users.noreply.github.com>
2026-08-20 12:06:39 +0200

    • Added the rest of the input fields for all the products attributes. • Fixed hover and focus effekt and other things with the styling. • Removed the SKU inputs because they are apparently auto-generated.
    Co-authored-by: Leo Leksell <leo98lxl@users.noreply.github.com>

--- app/components/product-form/product-form.tsx
+++ app/components/product-form/product-form.tsx
@@ -3,26 +3,11 @@ import "./product-form.css";
 import { Product } from "@/app/types";
 
 /* Product information attributes:
-    weight?: number;
-    dimensions?: {
-        width: number;
-        height: number;
-        depth: number;
-    };
-    warrantyInformation?: string;
-    shippingInformation?: string;
-    
-    
-    reviews?: {
-        rating: number;
-        comment: string;
-        date: string;
-        reviewerName: string;
-        reviewerEmail: string;
-    }[];
+
     returnPolicy?: string;
     minimumOrderQuantity?: number;
     meta: {
+        // The meta-data is auto-generated
         createdAt: string;
         updatedAt: string;
         barcode?: string;
@@ -44,19 +29,25 @@ export default function ProductForm()
 
             <form action="" method="post" className="product-form" >
 
+                <p>*Required fields</p>
+
+
                 {/* id: number; */}
+                {/* The ID-number is auto-generated */}
 
                 {/* title: string; */}
                 <div className="form-group">
-                    <label htmlFor="title">Title</label>
-                    <input type="text" id="title" name="title" placeholder="Product title (max 50 characters)" maxLength={50} required autoFocus />
+                    <label htmlFor="title"> Title (max 50 characters) </label>
+                    <input type="text" id="title" name="title" placeholder="Product title..." maxLength={50} required autoFocus />
                 </div>
+
 
                 {/* description: string; */}
                 <div className="form-group">
-                    <label htmlFor="description">Description</label>
-                    <input type="text" id="description" name="description" placeholder="Product description (max 250 characters)" maxLength={250} />
+                    <label htmlFor="description"> Description (max 250 characters)* </label>
+                    <input type="text" id="description" name="description" placeholder="Product description..." maxLength={250} required />
                 </div>
+
 
                 {/* categoryId: number; */}
 
@@ -93,77 +84,105 @@ export default function ProductForm()
                     </select>
                 </div>
 
+
                 {/* price: number; */}
                 <div className="form-group">
-                    <label htmlFor="price">Price in euros</label>
-                    <input type="number" id="price" name="price" placeholder="Price in €" />
+                    <label htmlFor="price">Price in euros (€)</label>
+                    <input type="number" id="price" name="price" placeholder="Product price..." required />
                 </div>
+
 
                 {/* discountPercentage?: number; */}
                 <div className="form-group">
-                    <label htmlFor="discountPercentage" > Discount in percent </label>
-                    <input type="number" id="discountPercentage" name="discountPercentage" placeholder="Discount in %" />
+                    <label htmlFor="discountPercentage" > Discount in percent (%) </label>
+                    <input type="number" id="discountPercentage" name="discountPercentage" placeholder="Product discount..." />
                 </div>
+
 
                 {/* rating?: number; */}
                 {/* <div className="form-group" >
                     <label htmlFor="rating" > Rating from 1-5 </label>
                     <input type="range" id="rating" name="rating" placeholder="Rating (1-5)" min={1} max={5} />
                 </div> */}
-                
+
+
                 {/* stock?: number; */}
                 <div className="form-group" >
                     <label htmlFor="stock" > Products in stock </label>
                     <input type="number" id="stock" name="stock" placeholder="Number of products in stock..." />
                 </div>
-                
+
 
                 {/* tags?: string[]; */}
-                <div className="form-group" >
-                    <label htmlFor="tags1" > Add 1-3 tags </label>
-                    <input type="text" id="tags1" name="tag1" placeholder="Tag 1 (required)" required />
+                <fieldset className="form-group" >
+                    <legend>Add 1-3 tags</legend>
 
+                    <label htmlFor="tag1" > Product tag 1 </label>
+                    <input type="text" id="tag1" name="tag1" placeholder="Product tag 1..." />
 
-                    <input type="text" name="tag2" placeholder="Tag 2 (optional)" />
-                    <input type="text" name="tag3" placeholder="Tag 3 (optional)" />
-                </div>
+                    <label htmlFor="tag2" > Product tag 2 </label>
+                    <input type="text" id="tag2" name="tag2" placeholder="Product tag 2..." />
+
+                    <label htmlFor="tag3" > Product tag 3 </label>
+                    <input type="text" id="tag3" name="tag3" placeholder="Product tag 3..." />
+                </fieldset>
+
 
                 {/* brand?: string; */}
                 <div className="form-group" >
-                    <label htmlFor="brand" > The products brand </label>
-                    <input type="text" id="brand" name="brand" placeholder="Product brand..." />
+                    <label htmlFor="brand" > Product brand </label>
+                    <input type="text" id="brand" name="brand" placeholder="The products brand..." />
                 </div>
+
 
                 {/* sku?: string; */}
+                {/* The SKU code is auto-generated */}
+
+
+                {/* weight?: number; */}
+                {/* Unknown weight unit in API. Guess it's kilograms */}
                 <div className="form-group" >
-                    <label htmlFor="sku" > Stock-keeping unit (SKU) </label>
-                    
-                    <div className="sku-input" >
-                        <input type="text" name="sku1" id="sku" placeholder="ABC"
-                            pattern="[A-Z]{3}" title="Three uppercase letters, A-Z" />
-                        <span> - </span>
-
-                        <input type="text" name="sku2" placeholder="DEF"
-                            pattern="[A-Z]{3}" title="Three uppercase letters, A-Z" />
-                        <span> - </span>
-
-                        <input type="text" name="sku3" placeholder="GHI"
-                            pattern="[A-Z]{3}" title="Three uppercase letters, A-Z" />
-                        <span> - </span>
-
-                        <input type="text" name="sku4" placeholder="123"
-                            pattern="[0-9]{3}" title="Three numbers, 0-9" />
-                    </div>
+                    <label htmlFor="weight" > Product weight in kilograms (kg) </label>
+                    <input type="number" id="weight" name="weight" placeholder="The products weight..." />
                 </div>
-                
 
 
-                
+                {/* dimensions?: {
+                    width: number;
+                    height: number;
+                    depth: number;
+                }; */}
+                <fieldset className="dimensions-fieldset form-group">
+                    <legend>Product dimensions</legend>
+
+                    <label htmlFor="width" > Width in centimeters (cm) </label>
+                    <input type="number" id="width" name="width" placeholder="Product width..." />
+
+                    <label htmlFor="height" > Height in centimeters (cm) </label>
+                    <input type="number" id="height" name="height" placeholder="Product height..." />
+
+                    <label htmlFor="depth" > Depth in centimeters (cm) </label>
+                    <input type="number" id="depth" name="depth" placeholder="Product depth..." />
+                </fieldset>
+
+
+                {/* warrantyInformation?: string; */}
+                <div className="form-group" >
+                    <label htmlFor="warrantyInfo" > Warranty information </label>
+                    <input type="text" id="warrantyInfo" name="warrantyInfo" placeholder="Information on the products warranty..." />
+                </div>
+
+                {/* shippingInformation?: string; */}
+                <div className="form-group" >
+                    <label htmlFor="shippingInfo" > Shipping information </label>
+                    <input type="text" id="shippingInfo" name="shippingInfo" placeholder="Information on how the product ships..." />
+                </div>
+
 
                 {/* availabilityStatus: string */}
-
                 <div className="form-group" >
-                    <label htmlFor="availability" > Availability </label>
+                    <label htmlFor="availability" > Product availability </label>
+
                     <select id="availability" name="availability" defaultValue="default">
                         <option value="default" disabled>Set availability</option>
                         <option value="in-stock">In Stock</option>
@@ -172,37 +191,75 @@ export default function ProductForm()
                     </select>
                 </div>
 
+
                 {/* reviews?: {
-        rating: number;
-        comment: string;
-        date: string;
-        reviewerName: string;
-        reviewerEmail: string;
-                }[]; */}
+                        rating: number;
+                        comment: string;
+                        date: string;
+                        reviewerName: string;
+                        reviewerEmail: string;
+                    }[];
+                */}
+                <fieldset className="form-group" >
+                    <legend>Product Review</legend>
 
-                <div className="form-group" >
-                    <h2>Product Review</h2>
-
-                    <label htmlFor="review-rating">Rating</label>
+                    <label htmlFor="review-rating"> Rating </label>
                     <input type="number" id="review-rating" name="review-rating" min="1" max="5" placeholder="Rating (1-5)" />
 
-                    <label htmlFor="review-comment">Comment</label>
+                    <label htmlFor="review-comment"> Comment </label>
                     <textarea id="review-comment" name="review-comment" placeholder="Leave a comment..."></textarea>
 
-                    <label htmlFor="review-date">Date</label>
-                    <input type="date" id="review-date" name="review-date" defaultValue="2026-08-14" />
+                    <label htmlFor="review-date"> Date </label>
+                    <input type="date" id="review-date" name="review-date" defaultValue="2026-08-20" />
 
-                    <label htmlFor="reviewer-name">Your name</label>
+                    <label htmlFor="reviewer-name"> Your name </label>
                     <input type="text" id="reviewer-name" name="reviewer-name" placeholder="Name of reviewer" />
 
-                    <label htmlFor="reviewer-email">Your email</label>
+                    <label htmlFor="reviewer-email"> Your email </label>
                     <input type="email" id="reviewer-email" name="reviewer-email" placeholder="Email of reviewer" />
-                </div>
+                </fieldset>
 
                 {/* returnPolicy?: string; */}
 
                 <div className="form-group" >
                     <label htmlFor="return-policy">Return policy</label>
+                    <select id="return-policy" name="return-policy" defaultValue="default">
+                        <option value="default" disabled>Select return policy</option>
+                        <option value="policy-none">No return policy</option>
+                        <option value="policy-7">7 days return policy</option>
+                        <option value="policy-30">30 days return policy</option>
+                        <option value="policy-60">60 days return policy</option>
+                        <option value="policy-90">90 days return policy</option>
+                    </select>
+                </div>
+
+                {/* minimumOrderQuantity?: number; */}
+
+                <div className="form-group" >
+                    <label htmlFor="min-order-quantity" >Order quantity (min)</label>
+                    <input type="number" id="min-order-quantity" name="min-order-quantity" min={0} defaultValue={0} />
+                </div>
+
+                {/* meta: {
+                // The meta-data is auto-generated
+                createdAt: string;
+                updatedAt: string;
+                barcode?: string;
+                qrCode?: string;
+                };*/}
+
+                {/* images: string[]; */}
+
+                <div className="form-group" >
+                    <label htmlFor="images" >Images</label>
+                    <input type="text" id="images" name="images" placeholder="Paste link to images" />
+                </div>
+
+                {/* thumbnail: string; */}
+
+                <div className="form-group" >
+                    <label htmlFor="thumbnail" >Thumbnail</label>
+                    <input type="text" id="thumbnail" name="thumbnail" placeholder="Paste link to thumbnail" />
                 </div>
 
                 {/* SUBMIT BUTTON */}
@@ -214,9 +271,27 @@ export default function ProductForm()
         </section>
     );
 }
-/* 
-                <div className="form-group" >
-                    <label htmlFor="generic" > generic form group </label>
-                    <input type="text" id="generic" name="generic" placeholder="..." />
-                </div>
- */
+
+/* sku?: string; */
+/* The SKU code is auto-generated */
+/*  <div className="form-group" >
+        <label htmlFor="sku" > Stock-keeping unit (SKU) </label>
+
+        <div className="sku-input" >
+            <input type="text" name="sku1" id="sku" placeholder="ABC"
+                pattern="[A-Z]{3}" title="Three uppercase letters, A-Z" />
+            <span> - </span>
+
+            <input type="text" name="sku2" placeholder="DEF"
+                pattern="[A-Z]{3}" title="Three uppercase letters, A-Z" />
+            <span> - </span>
+
+            <input type="text" name="sku3" placeholder="GHI"
+                pattern="[A-Z]{3}" title="Three uppercase letters, A-Z" />
+            <span> - </span>
+
+            <input type="text" name="sku4" placeholder="123"
+                pattern="[0-9]{3}" title="Three numbers, 0-9" />
+        </div>
+    </div>
+*/
