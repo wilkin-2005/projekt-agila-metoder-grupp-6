70ee1326bbef2f86675f85323af2f724920cf8d8
wilkin2005 <279715150+wilkin-2005@users.noreply.github.com>
2026-08-24 15:51:48 +0200

    Fixed some requested changes and a few other things: • The "stock" input field is now required. • Changed the id and name attributes for many of the input fields. • Removed some old code that was out-commented. • Added an "step" attribute to the price input and changed the "min" value from 1 to 0.1. • Changed the <form> to an <Form> tag from Next.js and removed the "method" attribute. • Other minor changes to make code more readable.

--- app/components/product-form/product-form.tsx
+++ app/components/product-form/product-form.tsx
@@ -1,4 +1,5 @@
 
+import Form from "next/form";
 import "./product-form.css";
 // import { Product, Category } from "@/app/types";
 
@@ -10,9 +11,9 @@ export default function ProductForm()
 
     return (
         <section aria-labelledby="form-header">
-            <h2 id="form-header">Add/Edit product</h2>
+            <h2 id="form-header" > Add/Edit product </h2>
 
-            <form action="" method="post" className="product-form" aria-labelledby="form-header" >
+            <Form action="" className="product-form" aria-labelledby="form-header" >
 
                 <p> <span className="required-text">*</span>Required fields</p>
 
@@ -34,7 +35,6 @@ export default function ProductForm()
                 {/* description: string; */}
                 <div className="form-group">
                     <label htmlFor="description"> Description (max 250 characters)<span className="required-text">*</span> </label>
-                    {/* <input type="text" id="description" name="description" placeholder="Product description..." maxLength={250} required /> */}
                     <textarea id="description" name="description" placeholder="Product description..." maxLength={250} rows={4} required />
                 </div>
 
@@ -97,7 +97,7 @@ export default function ProductForm()
 
                 {/* tags?: string[]; */}
                 <fieldset className="form-group" >
-                    <legend>Add 1-3 tags</legend>
+                    <legend> Add 1-3 tags </legend>
 
                     <label htmlFor="tag1" > Product tag 1 </label>
                     <input type="text" id="tag1" name="tag1" placeholder="Product tag 1..." maxLength={30} />
@@ -117,8 +117,7 @@ export default function ProductForm()
                 {/* price: number; */}
                 <div className="form-group">
                     <label htmlFor="price">Price in euros (€)<span className="required-text">*</span> </label>
-                    <input type="number" id="price" name="price" placeholder="Product price..." min={1} required />
-                    {/* TODO: make it possible to input decimal numbers. */}
+                    <input type="number" id="price" name="price" placeholder="Product price..." min={0.1} step={0.01} required />
                 </div>
 
 
@@ -131,17 +130,17 @@ export default function ProductForm()
 
                 {/* stock?: number; */}
                 <div className="form-group" >
-                    <label htmlFor="stock" > Products in stock </label>
-                    <input type="number" id="stock" name="stock" placeholder="Number of products in stock..." min={0} />
+                    <label htmlFor="stock" > Products in stock<span className="required-text">*</span> </label>
+                    <input type="number" id="stock" name="stock" placeholder="Number of products in stock..." min={0} required />
                 </div>
 
 
                 {/* availabilityStatus: string */}
                 <div className="form-group" >
-                    <label htmlFor="availability" > Product availability </label>
                     {/* Availability information should propably be calculated automatically from the stock number. */}
+                    <label htmlFor="availability" > Product availability </label>
 
-                    <select id="availability" name="availability" defaultValue="default">
+                    <select id="availability" name="availabilityStatus" defaultValue="default">
                         <option value="default" disabled>Set availability</option>
                         <option value="in-stock">In Stock</option>
                         <option value="low-stock">Low Stock</option>
@@ -169,7 +168,7 @@ export default function ProductForm()
                 }; */}
                 {/* Unkown length unit used in API. We say it's centimeters */}
                 <fieldset className="dimensions-fieldset form-group">
-                    <legend>Product dimensions</legend>
+                    <legend> Product dimensions </legend>
 
                     <label htmlFor="width" > Width in centimeters (cm) </label>
                     <input type="number" id="width"  name="width"  placeholder="Product width..."  min={1} />
@@ -188,9 +187,8 @@ export default function ProductForm()
 
                 {/* rating?: number; */}
                 {/* <div className="form-group" >
-                    <label htmlFor="rating" > Rating from 1-5 </label>
-                    <input type="range" id="rating" name="rating" placeholder="Rating (1-5)" min={1} max={5} />
-                    // Should be possible to have it in decimal numbers.
+                    <label htmlFor="productRating" > Rating of product, from 1-5 </label>
+                    <input type="range" id="productRating" name="rating" placeholder="Rating (1-5)" min={1} max={5} step={0.1} />
                 </div> */}
 
                 {/* reviews?: {
@@ -206,19 +204,19 @@ export default function ProductForm()
                     {/* Maybe doesn't make sense that the admin can add review information to a products. Maybe remove later? */}
 
                     <label htmlFor="review-rating"> Rating </label>
-                    <input type="number" id="review-rating" name="review-rating" min="1" max="5" placeholder="Rating (1-5)" />
+                    <input type="number" id="review-rating" name="rating" min="1" max="5" placeholder="Rating (1-5)" />
 
                     <label htmlFor="review-comment"> Comment (max 250 characters) </label>
-                    <textarea id="review-comment" name="review-comment" placeholder="Leave a comment..."  maxLength={250} rows={3} />
+                    <textarea id="review-comment" name="comment" placeholder="Leave a comment..."  maxLength={250} rows={3} />
 
                     <label htmlFor="review-date"> Review date </label>
-                    <input type="date" id="review-date" name="review-date" />
+                    <input type="date" id="review-date" name="date" />
 
                     <label htmlFor="reviewer-name"> Name of reviewer </label>
-                    <input type="text" id="reviewer-name" name="reviewer-name" placeholder="Your name..." />
+                    <input type="text" id="reviewer-name" name="reviewerName" placeholder="Your name..." />
 
                     <label htmlFor="reviewer-email"> Email of reviewer </label>
-                    <input type="email" id="reviewer-email" name="reviewer-email" placeholder="Your email address..." />
+                    <input type="email" id="reviewer-email" name="reviewerEmail" placeholder="Your email address..." />
                 </fieldset>
 
 
@@ -228,24 +226,23 @@ export default function ProductForm()
 
                 {/* minimumOrderQuantity?: number; */}
                 <div className="form-group" >
-                    <label htmlFor="min-order-quantity" > Minimum order quantity </label>
-                    <input type="number" id="min-order-quantity" name="min-order-quantity" placeholder="Minimum amount of products per order..." min={1} />
+                    <label htmlFor="minOrderQuantity" > Minimum order quantity </label>
+                    <input type="number" id="minOrderQuantity" name="minimumOrderQuantity" placeholder="Minimum amount of products per order..." min={1} />
                 </div>
 
 
                 {/* shippingInformation?: string; */}
                 <div className="form-group" >
                     <label htmlFor="shippingInfo" > Shipping information (max 150 characters) </label>
-                    {/* <input type="text" id="shippingInfo" name="shippingInfo" placeholder="Information on how the product ships..." maxLength={150} /> */}
-                    <textarea id="shippingInfo" name="descshippingInfoription" placeholder="Information on how the product ships..." maxLength={150} rows={2} />
+                    <textarea id="shippingInfo" name="shippingInformation" placeholder="Information on how the product ships..." maxLength={150} rows={2} />
                 </div>
 
 
                 {/* returnPolicy?: string; */}
                 <div className="form-group" >
-                    <label htmlFor="return-policy"> Return policy </label>
+                    <label htmlFor="returnPolicy"> Return policy </label>
 
-                    <select id="return-policy" name="return-policy" defaultValue="default">
+                    <select id="returnPolicy" name="returnPolicy" defaultValue="default">
                         <option value="default" disabled > Select return policy </option>
                         <option value="policy-none" >   No return policy </option>
                         <option value="policy-7" >  7 days return policy </option>
@@ -260,7 +257,7 @@ export default function ProductForm()
                 <div className="form-group" >
                     <label htmlFor="warrantyInfo" > Warranty information </label>
 
-                    <select id="warrantyInfo" name="warrantyInfo" defaultValue="default" >
+                    <select id="warrantyInfo" name="warrantyInformation" defaultValue="default" >
                         <option value="default" disabled > Select warranty length </option>
 
                         <option value="warranty-none" > No warranty </option>
@@ -316,12 +313,12 @@ export default function ProductForm()
 
                 {/* thumbnail: string; */}
                 <div className="form-group" >
-                    <label htmlFor="thumbnail" > Product thumbnail <span className="minor-info"> (preferably 300 x 300 pixels) </span> <span className="required-text">*</span> </label>
+                    <label htmlFor="thumbnail" > Product thumbnail <span className="minor-info"> (preferably 300 x 300 pixels)</span><span className="required-text">*</span> </label>
                     <input type="url" id="thumbnail" name="thumbnail" placeholder="Paste link to small thumbnail image..." required />
                 </div>
 
 
-                <span className="form-subheader"> {/* Used for seperation line */} </span>
+                <span className="form-subheader"> {/* Used only for seperation line */} </span>
                 {/* ========== ========== ========== ========== ========== ========== */}
 
 
@@ -340,18 +337,19 @@ export default function ProductForm()
 
                 {/* SUBMIT AND RESET BUTTONS */}
                 {/* <input type="reset" className="form-btn" value="Reset form" /> */}
+
                 <input type="submit" className="form-btn" value="Submit" />
+                {/* Submit button should say "Create" or "Edit" or something simular when used on their respective pages */}
 
-            </form>
-
+            </Form>
         </section>
     );
 }
 
+// Old SKU input fields:
 
-
-/* sku?: string; */
-/* The SKU code is auto-generated */
+// sku?: string;
+// The SKU code is auto-generated
 /*  <div className="form-group" >
         <label htmlFor="sku" > Stock-keeping unit (SKU) </label>
 
