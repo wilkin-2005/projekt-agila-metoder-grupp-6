b9c358e23d25246410dc742a712f81d1bcad8640
wilkin2005 <279715150+wilkin-2005@users.noreply.github.com>
2026-08-26 12:06:38 +0200

    Foundational work on the Edit product funtionality. • Created a component for the edit product form and copied the entire generic product form into it. • Added that component to the edit "page.tsx" which I also restructured. • Renamed the Edit page's CSS-file from "page.css" to "edit-product-page.css". • Made a few small changes to the product form from the original generic form. I changed the title and submit button text and removed a code comment.

--- app/product/edit/[id]/page.tsx
+++ app/product/edit/[id]/page.tsx
@@ -1,6 +1,11 @@
-import './page.css'
 
-export default async function Page({params}: {params: Promise<{ id: string }>}) {
+import "./edit-product-page.css";
+import EditProductForm from "@/app/components/product-form/edit-product-form";
+
+
+//
+export default async function EditProductPage( {params}: { params: Promise<{ id: string }> } )
+{
     const { id } = await params;
 
     // get product with id from api here
@@ -8,17 +13,18 @@ export default async function Page({params}: {params: Promise<{ id: string }>}) 
   return (
     <main className="EditProduct">
         <section className="centered-section">
+
             <div className='EditProduct__Header'>
-        <h1 className="EditProduct__Title">
-            Edit product
-        </h1>
-        <a href='/'>Go back to products list</a>
-        </div>
-        <section>
-            {
-                /* add product form here */
-            }
-        </section>
+                <h1 className="EditProduct__Title"> Edit product </h1>
+                <a href='/'>Go back to products list</a>
+            </div>
+
+            <section>
+
+                <EditProductForm />
+
+            </section>
+
         </section>
     </main>
   );
